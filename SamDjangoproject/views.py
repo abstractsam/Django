ebb9fa528ad2476a4fbe197454d10ec9695f4e0b
eMobilis <samuelnjihia81@gmail.com>
2023-07-18 09:41:16 +0300

    first commit

---
+++ SamDjangoproject/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+
+def indexpage(request):
+    return render(request, "index.html")
+
+def aboutpage(request):
+    return render(request, "About.html")
+
+
+def servpage(request):
+    return render(request, "Services.html")
+
+
+def blogpage(request):
+    return render(request, "Blog.html")
+
+
+def projpage(request):
+    return render(request, "Project.html")
